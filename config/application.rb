59f81a35f14c12aea279a9d540a644176af9614b
daveholmes88 <github email address>
2021-01-08 17:39:47 -0600

    ending Friday

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,13 @@ module ShopifyBackEnd
     # the framework and any gems in your application.
   end
 end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*"
+ 
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
